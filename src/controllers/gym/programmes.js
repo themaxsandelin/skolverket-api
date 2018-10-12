9181bb7505a6386568fe0258ffb52c05dc09e9d8
Max Sandelin <themaxsandelin@gmail.com>
2018-10-12 08:47:35 +0200

    Finalized gymnasie-level data parsing.

--- src/controllers/gym/programmes.js
+++ src/controllers/gym/programmes.js
@@ -15,6 +15,7 @@ const rootPath = path.resolve(__dirname + '/../../../files/syllabus')
 
 router.get('/', (req, res) => {
   const file = rootPath + '/programsAndOrientations/Program_och_inriktningar.xml'
+  const fourthTechYearExists = fs.existsSync(rootPath + '/Gymnasieingenjor.xml')
 
   parseString(fs.readFileSync(file).toString(), (err, result) => {
     const programs = []
@@ -43,110 +44,136 @@ router.get('/', (req, res) => {
       })
     })
 
-    res.json(programs)
+    if (fourthTechYearExists) {
+      parseString(fs.readFileSync(rootPath + '/Gymnasieingenjor.xml').toString(), (err, result) => {
+        const program = result.fourthTechnicalYear
+        programs.push({
+          name: program.name[0],
+          code: program.code[0],
+          orientaions: []
+        })
+
+        programs.sort((a, b) => {
+          if (a.code > b.code) return 1
+          if (a.code < b.code) return -1
+          return 0
+        })
+
+        res.json(programs)
+      })
+    } else {
+      res.json(programs)
+    }
   })
 })
 
 router.get('/:code', (req, res) => {
   const code = req.params.code.toLowerCase()
   const file = rootPath + '/programsAndOrientations/Program_och_inriktningar.xml'
+  const fourthTechYear = fs.existsSync(rootPath + '/Gymnasieingenjor.xml') ? rootPath + '/Gymnasieingenjor.xml' : ''
 
-  parseString(fs.readFileSync(file).toString(), (err, result) => {
-    let validCode = false
-    result.ProgramsAndOrientations.program.forEach(progr => {
-      if (progr.code[0].toLowerCase() === code) {
-        validCode = true
-        const programme = progr
-        // Construct file name based on the name of the programme.
-        let file = rootPath + '/program/' + latinize(progr.name[0]) + '.xml'
-        while (file.indexOf('–') > -1) {
-          file = file.replace('–', '-')
-        }
-        // Read the programme specific XML file to get it's data.
-        parseString(fs.readFileSync(file), (err, result) => {
-          const data = result.program
-
-          const programme = {
-            name: data.name[0],
-            code: data.code[0],
-            skolfs: data.skolfsId[0],
-            applianceDate: data.applianceDate[0],
-            typeOfSchooling: data.typeOfSchooling[0],
-            typeOfProgram: data.typeOfProgram[0],
-            generalSubjects: [],
-            programmeSubjects: [],
-            specializationSubjects: [],
-            orientations: [],
-            profiles: [],
-            individualChoice: [],
-            projectAssignments: [],
-            professionalDegrees: []
-          }
-
-          // Go through "commonMandatory" to get all general subjects.
-          if (data.commonMandatory[0] !== '') {
-            programme.generalSubjects = formatter.formatSubjects(data.commonMandatory[0].subject)
-          }
-
-          // Go through "commonProgram" to get all programme specific subjects.
-          if (data.commonProgram[0] !== '') {
-            programme.programmeSubjects = formatter.formatSubjects(data.commonProgram[0].subject)
-          }
-
-          // Go through all "programOrientations" to get programme orientations.
-          if (data.programOrientations[0] !== '') {
-            data.programOrientations[0].programOrientation.forEach(orientationData => {
-              const orientation = {
-                name: orientationData.name[0],
-                code: orientationData.code[0],
-                points: parseInt(orientationData.point[0]),
-                subjects: (orientationData.subject[0] !== '') ? formatter.formatSubjects(orientationData.subject) : []
-              }
-              programme.orientations.push(orientation)
-            })
-          }
-
-          if (data.specialization[0] !== '') {
-            programme.specializationSubjects = formatter.formatSubjects(data.specialization[0].subject)
-          }
-
-          // Go through "professionalDegrees"
-          if (data.professionalDegrees[0] !== '') {
-            data.professionalDegrees[0].professionalDegree.forEach(degreeData => {
-              const degree = {
-                name: degreeData.name[0],
-                code: degreeData.programOrientationCode[0],
-                subjects: (degreeData.subject && degreeData.subject[0] !== '') ? formatter.formatSubjects(degreeData.subject) : []
-              }
-
-              programme.professionalDegrees.push(degree)
-            })
-          }
-
-          // Go through "projectAssignments"
-          if (data.projectAssignment[0] !== '') {
-            data.projectAssignment[0].subject.forEach(subjectData => {
-              programme.projectAssignments.push({
-                name: subjectData.name[0],
-                code: subjectData.code[0],
-                optional: subjectData.optional[0] === 'true' ? true : false,
-                alias: subjectData.alias[0] === 'true' ? true : false
-              })
-            })
-          }
-
-          // Go through "profiles"
-          if (data.profiles[0] !== '') {
-            programme.profiles = formatter.formatProfiles(data.profiles.profile)
-          }
-
-          res.json(programme)
-        })
-      }
+  let validCode = false
+  if (code === 'vi001') {
+    validCode = true
+    parseString(fs.readFileSync(fourthTechYear).toString(), (err, result) => {
+      res.json( formatter.formatProgramme(result.fourthTechnicalYear) )
     })
+  } else {
+    parseString(fs.readFileSync(file).toString(), (err, result) => {
+      result.ProgramsAndOrientations.program.forEach(progr => {
+        if (progr.code[0].toLowerCase() === code) {
+          validCode = true
+          const programme = progr
+          // Construct file name based on the name of the programme.
+          let file = rootPath + '/program/' + latinize(progr.name[0]) + '.xml'
+          while (file.indexOf('–') > -1) {
+            file = file.replace('–', '-')
+          }
+          // Read the programme specific XML file to get it's data.
+          parseString(fs.readFileSync(file), (err, result) => {
+            const data = result.program
 
-    if (!validCode) res.status(404).send('Not found.')
-  })
+            const programme = {
+              name: data.name[0],
+              code: data.code[0],
+              skolfs: data.skolfsId[0],
+              applianceDate: data.applianceDate[0],
+              typeOfSchooling: data.typeOfSchooling[0],
+              typeOfProgram: data.typeOfProgram[0],
+              generalSubjects: [],
+              programmeSubjects: [],
+              specializationSubjects: [],
+              orientations: [],
+              profiles: [],
+              individualChoice: [],
+              projectAssignments: [],
+              professionalDegrees: []
+            }
+
+            // Go through "commonMandatory" to get all general subjects.
+            if (data.commonMandatory[0] !== '') {
+              programme.generalSubjects = formatter.formatSubjects(data.commonMandatory[0].subject)
+            }
+
+            // Go through "commonProgram" to get all programme specific subjects.
+            if (data.commonProgram[0] !== '') {
+              programme.programmeSubjects = formatter.formatSubjects(data.commonProgram[0].subject)
+            }
+
+            // Go through all "programOrientations" to get programme orientations.
+            if (data.programOrientations[0] !== '') {
+              data.programOrientations[0].programOrientation.forEach(orientationData => {
+                const orientation = {
+                  name: orientationData.name[0],
+                  code: orientationData.code[0],
+                  points: parseInt(orientationData.point[0]),
+                  subjects: (orientationData.subject[0] !== '') ? formatter.formatSubjects(orientationData.subject) : []
+                }
+                programme.orientations.push(orientation)
+              })
+            }
+
+            if (data.specialization[0] !== '') {
+              programme.specializationSubjects = formatter.formatSubjects(data.specialization[0].subject)
+            }
+
+            // Go through "professionalDegrees"
+            if (data.professionalDegrees[0] !== '') {
+              data.professionalDegrees[0].professionalDegree.forEach(degreeData => {
+                const degree = {
+                  name: degreeData.name[0],
+                  code: degreeData.programOrientationCode[0],
+                  subjects: (degreeData.subject && degreeData.subject[0] !== '') ? formatter.formatSubjects(degreeData.subject) : []
+                }
+
+                programme.professionalDegrees.push(degree)
+              })
+            }
+
+            // Go through "projectAssignments"
+            if (data.projectAssignment[0] !== '') {
+              data.projectAssignment[0].subject.forEach(subjectData => {
+                programme.projectAssignments.push({
+                  name: subjectData.name[0],
+                  code: subjectData.code[0],
+                  optional: subjectData.optional[0] === 'true' ? true : false,
+                  alias: subjectData.alias[0] === 'true' ? true : false
+                })
+              })
+            }
+
+            // Go through "profiles"
+            if (data.profiles[0] !== '') {
+              programme.profiles = formatter.formatProfiles(data.profiles.profile)
+            }
+
+            res.json(formatter.formatProgramme(data))
+          })
+        }
+      })
+      if (!validCode) res.status(404).send('Not found.')
+    })
+  }
 })
 
 module.exports = router
--- src/helpers/formatter.js
+++ src/helpers/formatter.js
@@ -4,18 +4,146 @@ const Turndown = require('turndown')
 function Formatter() {
   const turndownService = new Turndown()
 
+  function formatProgramme(data) {
+    const programme = {
+      name: data.name[0],
+      code: data.code[0],
+      skolfs: data.skolfsId[0],
+      applianceDate: data.applianceDate[0],
+      typeOfSchooling: data.typeOfSchooling[0],
+      typeOfProgram: data.typeOfProgram[0],
+      purpose: {},
+      generalSubjects: [],
+      programmeSubjects: [],
+      specializationSubjects: [],
+      orientations: [],
+      profiles: [],
+      individualChoice: [],
+      projectAssignments: [],
+      professionalDegrees: []
+    }
+
+    if (data.purpose[0].degreeObjective && data.purpose[0].degreeObjective[0] !== '') {
+      programme.purpose.degreeObjective = {
+        title: (typeof data.purpose[0].degreeObjective[0].title[0] === 'string') ? data.purpose[0].degreeObjective[0].title[0] : '',
+        content: turndownService.turndown(data.purpose[0].degreeObjective[0].content[0])
+      }
+    }
+
+    if (data.purpose[0].orientation && data.purpose[0].orientation[0] !== '') {
+      programme.purpose.orientation = {
+        title: (typeof data.purpose[0].orientation[0].title[0] === 'string') ? data.purpose[0].orientation[0].title[0] : '',
+        content: turndownService.turndown(data.purpose[0].orientation[0].content[0])
+      }
+    }
+
+    if (data.purpose[0].educationObjective && data.purpose[0].educationObjective[0] !== '') {
+      programme.purpose.educationObjective = {
+        title: (typeof data.purpose[0].educationObjective[0].title[0] === 'string') ? data.purpose[0].educationObjective[0].title[0] : '',
+        content: turndownService.turndown(data.purpose[0].educationObjective[0].content[0])
+      }
+    }
+
+    // Go through "commonMandatory" to get all general subjects.
+    if (data.commonMandatory && data.commonMandatory[0] !== '') {
+      programme.generalSubjects = formatSubjects(data.commonMandatory[0].subject)
+    }
+
+    // Go through "commonProgram" to get all programme specific subjects.
+    if (data.commonProgram && data.commonProgram[0] !== '') {
+      programme.programmeSubjects = formatSubjects(data.commonProgram[0].subject)
+    }
+
+    // Go through all "programOrientations" to get programme orientations.
+    if (data.programOrientations && data.programOrientations[0] !== '') {
+      data.programOrientations[0].programOrientation.forEach(orientationData => {
+        const orientation = {
+          name: orientationData.name[0],
+          code: orientationData.code[0],
+          points: parseInt(orientationData.point[0]),
+          subjects: (orientationData.subject[0] !== '') ? formatSubjects(orientationData.subject) : []
+        }
+        programme.orientations.push(orientation)
+      })
+    }
+
+    if (data.specialization && data.specialization[0] !== '') {
+      programme.specializationSubjects = formatSubjects(data.specialization[0].subject)
+    }
+
+    // Go through "professionalDegrees"
+    if (data.professionalDegrees && data.professionalDegrees[0] !== '') {
+      data.professionalDegrees[0].professionalDegree.forEach(degreeData => {
+        const degree = {
+          name: degreeData.name[0],
+          code: degreeData.programOrientationCode[0],
+          subjects: (degreeData.subject && degreeData.subject[0] !== '') ? formatSubjects(degreeData.subject) : []
+        }
+
+        programme.professionalDegrees.push(degree)
+      })
+    }
+
+    // Go through "projectAssignments"
+    if (data.projectAssignment && data.projectAssignment[0] !== '') {
+      data.projectAssignment[0].subject.forEach(subjectData => {
+        programme.projectAssignments.push({
+          name: subjectData.name[0],
+          code: subjectData.code[0],
+          optional: subjectData.optional[0] === 'true' ? true : false,
+          alias: subjectData.alias[0] === 'true' ? true : false
+        })
+      })
+    }
+
+    // Go through "profiles"
+    if (data.profiles && data.profiles[0] !== '') {
+      programme.profiles = formatProfiles(data.profiles[0].profile)
+    }
+
+    return programme
+  }
+
   function formatProfiles(profileList) {
     const profiles = []
     profileList.forEach(profileData => {
-      profiles.push({
+      const profile = {
         name: profileData.name[0],
         code: profileData.code[0],
-        points: parseInt(profileData.points[0]),
-        subjects: formatSubjects(profileData.subject)
-      })
+        points: parseInt(profileData.points[0])
+      }
+      if (profileData.subject) {
+        profile.subjects = formatSubjects(profileData.subject)
+      }
+      if (profileData.profileExits) {
+        profile.exits = formatProfileExits(profileData.profileExits[0].profileExit)
+      }
+
+      profiles.push(profile)
     })
 
     return profiles
+  }
+
+  function formatProfileExits(exitList) {
+    const exits = []
+
+    exitList.forEach(exitData => {
+      const exit = {
+        name: exitData.name[0],
+        points: parseInt(exitData.points[0])
+      }
+      if (exitData.subject) {
+        exit.subjects = formatSubjects(exitData.subject)
+      }
+      if (exitData.optionalCourses) {
+        exit.optionalCourses = formatSubjects(exitData.optionalCourses[0].subject)
+      }
+
+      exits.push(exit)
+    })
+
+    return exits
   }
 
   function formatSubjects(subjectList) {
@@ -26,9 +154,15 @@ function Formatter() {
         name: subjectData.name[0],
         code: subjectData.code[0]
       }
-      if (subjectData.courses) subject.courses = formatCourses(subjectData.courses)
-      if (subjectData.course) subject.courses = formatCourses(subjectData.course)
-      if (subjectData.point) subject.points = parseInt(subjectData.point[0])
+      if (subjectData.point) {
+        subject.points = parseInt(subjectData.point[0])
+      }
+      if (subjectData.optional) {
+        subject.optional = (subjectData.optional[0] === 'true')
+      }
+      if (subjectData.courses || subjectData.course) {
+        subject.courses = formatCourses(subjectData.courses ? subjectData.courses : subjectData.course)
+      }
 
       subjects.push(subject)
     })
@@ -71,6 +205,7 @@ function Formatter() {
 
 
   return {
+    formatProgramme,
     formatProfiles,
     formatCourses,
     formatCourseData,
